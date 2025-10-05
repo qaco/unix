8f0047af55e321ecd666ce1cf6469019c061718a
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-10-05 18:10:16 +0200

    ci: setup the GitHub workflow

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#! /bin/bash
-
-ln -s ./bashrc ~/.bashrc
-cp .bash_aliases ~/.bash_aliases
-ln -s .gitconfig ~/.gitconfig
+#!/usr/bin/env bash
+set -euo pipefail
+R="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+ln -sfn -- "$R/.bashrc"       "$HOME/.bashrc"
+cp      -- "$R/.bash_aliases" "$HOME/.bash_aliases"
+ln -sfn -- "$R/.gitconfig"    "$HOME/.gitconfig"
